a04bf24b6a1409435bdcde9dfc36ab1965f53f96
patrickhaahr <git@haahr.me>
2026-05-02 01:24:02 +0200

    fix(hosts): nika hardware config

--- modules/aspects/host/nika-hardware.nix
+++ modules/aspects/host/nika-hardware.nix
@@ -3,24 +3,22 @@
     imports = [
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
-    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "thunderbolt" "usbhid" "uas" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-intel" ];
+    boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
     fileSystems."/" = {
-      device = "/dev/mapper/luks-4b887d46-af7b-4153-9105-3f9286df520d";
+      device = "/dev/mapper/luks-1dbf142a-5b0c-430b-a372-846e8c50f8d7";
       fsType = "ext4";
     };
-    boot.initrd.luks.devices."luks-4b887d46-af7b-4153-9105-3f9286df520d".device =
-      "/dev/disk/by-uuid/4b887d46-af7b-4153-9105-3f9286df520d";
+    boot.initrd.luks.devices."luks-1dbf142a-5b0c-430b-a372-846e8c50f8d7".device = "/dev/disk/by-uuid/1dbf142a-5b0c-430b-a372-846e8c50f8d7";
     fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/6348-8E7C";
+      device = "/dev/disk/by-uuid/87D0-BE1B";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
     swapDevices = [ ];
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.intel.updateMicrocode =
-      lib.mkDefault config.hardware.enableRedistributableFirmware;
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
