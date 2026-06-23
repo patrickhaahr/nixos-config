d6d3f917dfdcdffacad4d96e596d054c2d2ef362
patrickhaahr <git@haahr.me>
2026-06-23 13:03:00 +0000

    feat(imu): enable tailscale

--- modules/aspects/host/imu.nix
+++ modules/aspects/host/imu.nix
@@ -5,6 +5,7 @@
       self.modules.nixos.nix-features
       self.modules.nixos.home-manager
       self.modules.nixos.identity-ph-headless
+      self.modules.nixos.tailscale
     ];
 
     networking.hostName = "imu";
