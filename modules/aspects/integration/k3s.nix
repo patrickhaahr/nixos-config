6e095590302da4abf4e66336faa1ff0a0a5c62bf
patrickhaahr <git@haahr.me>
2026-07-01 14:13:22 +0200

    fix(k3s): allow ph to read kubeconfig

--- modules/aspects/integration/k3s.nix
+++ modules/aspects/integration/k3s.nix
@@ -1,5 +1,9 @@
 { ... }: {
   flake.modules.nixos.k3s = {
+    users.groups.k3s-admin = { };
+
+    users.users.ph.extraGroups = [ "k3s-admin" ];
+
     networking.firewall.trustedInterfaces = [
       "cni0"
       "flannel.1"
@@ -14,7 +18,11 @@
     services.k3s = {
       enable = true;
       role = "server";
-      extraFlags = [ "--node-ip=10.0.20.3" ];
+      extraFlags = [
+        "--node-ip=10.0.20.3"
+        "--write-kubeconfig-group=k3s-admin"
+        "--write-kubeconfig-mode=0640"
+      ];
     };
   };
 }
