1eb3c97ae5f3cbf8b81065fa43fdeb0e31cfa7d2
patrickhaahr <git@haahr.me>
2026-07-18 19:01:01 +0200

    feat(loki): use unstable nixos and latest kernel

--- modules/aspects/host/loki.nix
+++ modules/aspects/host/loki.nix
@@ -24,6 +24,7 @@
       console.keyMap = "dk-latin1";
       nixpkgs.config.allowUnfree = true;
 
+      boot.kernelPackages = pkgs.linuxPackages_latest;
       boot.loader = {
         efi.canTouchEfiVariables = true;
         systemd-boot.enable = true;
--- modules/hosts/loki/default.nix
+++ modules/hosts/loki/default.nix
@@ -1,5 +1,5 @@
 { self, inputs, ... }: {
-  flake.nixosConfigurations.loki = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.loki = inputs.nixpkgs-unstable.lib.nixosSystem {
     modules = [
       self.modules.nixos.loki
     ];
