69cd4f1a5c907ee85b44300f0206902eafc1e8aa
patrickhaahr <git@haahr.me>
2026-08-16 13:35:53 +0200

    feat(herdr): use Nushell and system toasts

--- modules/aspects/cli/herdr.nix
+++ modules/aspects/cli/herdr.nix
@@ -1,5 +1,12 @@
 { inputs, ... }: {
   flake.modules.homeManager.herdr = { pkgs, ... }: {
     home.packages = [ inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+    xdg.configFile."herdr/config.toml".text = ''
+      [terminal]
+      default_shell = "nu"
+
+      [ui.toast]
+      delivery = "system"
+    '';
   };
 }
