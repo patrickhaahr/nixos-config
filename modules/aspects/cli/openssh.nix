18e802ee718201d0156d01d40b5c84507cb4b1af
patrickhaahr <git@haahr.me>
2026-08-24 23:21:00 +0200

    fix(ssh): drop materializeSshConfig hook to keep HM-managed symlink

--- modules/aspects/cli/openssh.nix
+++ modules/aspects/cli/openssh.nix
@@ -20,68 +20,57 @@ _: {
     };
   };
 
-  flake.modules.homeManager.openssh =
-    { lib, pkgs, ... }:
-    {
-      programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
-        settings = {
-          "*" = {
-            ForwardAgent = false;
-            AddKeysToAgent = "no";
-            Compression = false;
-            ServerAliveInterval = 0;
-            ServerAliveCountMax = 3;
-            HashKnownHosts = false;
-            UserKnownHostsFile = "~/.ssh/known_hosts";
-            ControlMaster = "no";
-            ControlPath = "~/.ssh/master-%r@%n:%p";
-            ControlPersist = "no";
-          };
-          "github.com" = {
-            HostName = "github.com";
-            User = "git";
-            IdentityAgent = "none";
-            IdentityFile = [
-              "~/.ssh/id_ed25519_sk_yk1"
-              "~/.ssh/id_ed25519_sk_yk2"
-              "~/.ssh/id_ed25519_github"
-            ];
-            IdentitiesOnly = true;
-          };
-          "uranus" = {
-            HostName = "100.118.180.74";
-            User = "root";
-            IdentityAgent = "none";
-            IdentityFile = "~/.ssh/id_ed25519_uranus";
-            IdentitiesOnly = true;
-          };
-          "zaza" = {
-            User = "ph";
-            IdentityAgent = "none";
-            IdentityFile = "~/.ssh/id_ed25519_zaza";
-            IdentitiesOnly = true;
-          };
-          "hermes" = {
-            HostName = "zaza";
-            User = "hermes";
-            IdentityAgent = "none";
-            IdentityFile = "~/.ssh/nika-to-hermes-auto";
-            IdentitiesOnly = true;
-          };
+  flake.modules.homeManager.openssh = _: {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      settings = {
+        "*" = {
+          ForwardAgent = false;
+          AddKeysToAgent = "no";
+          Compression = false;
+          ServerAliveInterval = 0;
+          ServerAliveCountMax = 3;
+          HashKnownHosts = false;
+          UserKnownHostsFile = "~/.ssh/known_hosts";
+          ControlMaster = "no";
+          ControlPath = "~/.ssh/master-%r@%n:%p";
+          ControlPersist = "no";
+        };
+        "github.com" = {
+          HostName = "github.com";
+          User = "git";
+          IdentityAgent = "none";
+          IdentityFile = [
+            "~/.ssh/id_ed25519_sk_yk1"
+            "~/.ssh/id_ed25519_sk_yk2"
+            "~/.ssh/id_ed25519_github"
+          ];
+          IdentitiesOnly = true;
+        };
+        "uranus" = {
+          HostName = "100.118.180.74";
+          User = "root";
+          IdentityAgent = "none";
+          IdentityFile = "~/.ssh/id_ed25519_uranus";
+          IdentitiesOnly = true;
+        };
+        "zaza" = {
+          User = "ph";
+          IdentityAgent = "none";
+          IdentityFile = "~/.ssh/id_ed25519_zaza";
+          IdentitiesOnly = true;
+        };
+        "hermes" = {
+          HostName = "zaza";
+          User = "hermes";
+          IdentityAgent = "none";
+          IdentityFile = "~/.ssh/nika-to-hermes-auto";
+          IdentitiesOnly = true;
         };
       };
-
-      home.activation.materializeSshConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        cfg="$HOME/.ssh/config"
-        if [ -L "$cfg" ]; then
-          src="$(readlink "$cfg")"
-          rm "$cfg"
-          ${pkgs.coreutils}/bin/install -m 600 "$src" "$cfg"
-        fi
-      '';
-
-      services.ssh-agent.enable = true;
     };
+
+    services.ssh-agent.enable = true;
+  };
 }
