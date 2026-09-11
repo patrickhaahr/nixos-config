7eeb161f5fbd96b21fbce932795c69740d1504be
hermes <hermes@haahr.me>
2026-09-11 22:10:11 +0200

    feat(hermes): add nix-ld

--- modules/aspects/host/zaza.nix
+++ modules/aspects/host/zaza.nix
@@ -4,6 +4,7 @@
       self.modules.nixos.zaza-hardware
       self.modules.nixos.doas
       self.modules.nixos.nix-features
+      self.modules.nixos.nix-ld
       self.modules.nixos.home-manager
       self.modules.nixos.homelab-excalidraw
       self.modules.nixos.homelab-executor
