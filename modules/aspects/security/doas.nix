2c13fc4dd3b92daa0d56b569e9086775ef056f2b
patrickhaahr <git@haahr.me>
2026-09-20 20:06:41 +0200

    feat(doas): let wheel users power off without polkit auth

--- modules/aspects/security/doas.nix
+++ modules/aspects/security/doas.nix
@@ -1,7 +1,22 @@
 _: {
   flake.modules.nixos.doas = {
-    security.sudo.enable = false;
-    security.doas.enable = true;
+    security = {
+      sudo.enable = false;
+      doas.enable = true;
+      polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (subject.isInGroup("wheel") &&
+              (action.id == "org.freedesktop.login1.power-off" ||
+               action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
+               action.id == "org.freedesktop.login1.reboot" ||
+               action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+               action.id == "org.freedesktop.login1.halt" ||
+               action.id == "org.freedesktop.login1.halt-multiple-sessions")) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    };
     system.tools.nixos-rebuild.enableRun0Elevation = true;
   };
 }
